6182c55545eb3b24cf8f3f7fadeaa4ca74b8432f
Sergey Z <cergeypro703@gmail.com>
2024-03-08 14:19:00 +0500

    argparse

--- smtp/client.py
+++ smtp/client.py
@@ -1,3 +1,4 @@
+import argparse
 import socket
 import ssl
 import time
@@ -14,8 +15,9 @@ def request(message: str) -> bytes:
 
 
 class SMTP:
-    def __init__(self, client: socket.socket):
+    def __init__(self, client: socket.socket, config: Config):
         self._client = client
+        self._config = config
 
     def hello(self) -> 'SMTP':
         self.__send('EHLO server\r\n')
@@ -48,20 +50,21 @@ class SMTP:
         self.__receive()
 
     def __receive(self):
-        print('Server:', self._client.recv(65535)
-              .decode().removesuffix('\n'))
+        if self._config.verbose:
+            print('Server:', self._client.recv(65535)
+                  .decode().removesuffix('\n'))
 
     def quit(self):
         self.__send('QUIT')
 
 
-def create_message(config: Config) -> str:
+def create_message(smpt: SMTP) -> str:
     # subject = config.subject
-    message = Message(config)
-    if not config.attachments:
+    message = Message(smpt._config)
+    if not smpt._config.attachments:
         attachments = get_attachments(os.getcwd())
     else:
-        attachments = get_attachments(config.attachments[0])
+        attachments = get_attachments(smpt._config.attachments[0])
 
     for attachment in attachments:
         message.append(attachment)
@@ -69,28 +72,56 @@ def create_message(config: Config) -> str:
 
     return f'{message.content}\r\n.\n'
 
+
 def get_attachments(path: str) -> list:
     attachments = []
     print(f"path: {path}")
     print(f"listdir: {os.listdir(str(path))}")
     for filename in os.listdir(str(path)):
-        if filename.lower().endswith(('.jpg','.jpeg', '.gif')):
-            attachments.append(Attachment(os.path.abspath(path + filename)).content)
+        if filename.lower().endswith(('.jpg', '.jpeg', '.gif')):
+            attachments.append(Attachment(
+                os.path.abspath(path + filename)).content)
     return attachments
 
 
 def main():
-    config = Config('smtp/config.json')
+    parser = argparse.ArgumentParser(
+        description='Send an email with attachments.')
+    parser.add_argument('--config', type=str, default='',
+                        help='load configuration from JSON file')
+    parser.add_argument('-s', '--server', type=str,
+                        help='SMTP server address or domain name')
+    parser.add_argument('-t', '--to', type=str,
+                        help='email address of the recipient')
+    parser.add_argument('-f', '--from_email', type=str,
+                        default='<>', help='email address of the sender')
+    parser.add_argument('--subject', type=str,
+                        default='Happy Pictures', help='subject of the email')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='display protocol communication')
+    parser.add_argument('-d', '--directory', type=str, default=os.getcwd(),
+                        help='directory containing images to be attached')
+
+    args = parser.parse_args()
+
+    if args.config:
+        # config = Config(args.config)
+        config = Config("config.json")
+    else:
+        config = Config(mail_server=args.server,  mail=args.from_email, fake_name=args.fake_name, name=args.name, password=args.password, recipients=[
+                        args.to], message_file=args.message_file, subject=args.subject, attachments=[args.directory], verbose=bool(args.verbose))
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.connect((config.mail_server, config.port))
         client = ssl.SSLContext().wrap_socket(sock=client)
 
-    smtp = SMTP(client)
+    smtp = SMTP(client, config)
     smtp.hello()\
         .authorisation(config.name, config.password)\
         .build_header(config.mail, config.recipients)\
-        .send_data(create_message(config))\
+        .send_data(create_message(smtp))\
         .quit()
+    client.close()
 
 
 if __name__ == '__main__':
--- smtp/config.py
+++ smtp/config.py
@@ -2,16 +2,31 @@ import json
 
 
 class Config:
-    def __init__(self, filename: str):
-        with open(filename, encoding='utf8') as config:
-            conf = json.load(config)
-            self.mail_server = conf['mail_server']
-            self.port = conf['port']
-            self.mail = conf['mail']
-            self.fake_name = conf['name']
-            self.name = conf['name']
-            self.password = conf['password']
-            self.recipients = conf['recipients']
-            self.message_file = conf['message_file']
-            self.subject = conf['subject']
-            self.attachments = conf['attachments']
+    def __init__(self, filename=''):
+        if filename:
+            with open(filename, encoding='utf8') as config:
+                conf = json.load(config)
+                self.mail_server = conf['mail_server'].split(':')[0]
+                self.port = int(conf['mail_server'].split(':')[-1])
+                self.mail = conf['mail']
+                self.fake_name = conf['name']
+                self.name = conf['name']
+                self.password = conf['password']
+                self.recipients = conf['recipients']
+                self.message_file = conf['message_file']
+                self.subject = conf['subject']
+                self.attachments = conf['attachments']
+                self.verbose = bool(conf['verbose'])
+                
+        else:
+            self.mail_server = ''
+            self.port = 0
+            self.mail = ''
+            self.fake_name = ''
+            self.name = ''
+            self.password = ''
+            self.recipients = []
+            self.message_file = ''
+            self.subject = ''
+            self.attachments = []
+            self.verbose = False
