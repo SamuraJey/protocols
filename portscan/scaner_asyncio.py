a8e7fd8b55d7e709a1fb34335b6014d7dd17ebe1
Sergey Z <cergeypro703@gmail.com>
2024-04-23 22:31:18 +0500

    cringe try-except, but it works.

--- portscan/scaner_asyncio.py
+++ portscan/scaner_asyncio.py
@@ -3,9 +3,10 @@ import socket
 import asyncio
 import time
 
-async def tcp_scanner(port, host):
+async def tcp_scanner(port: int, host: str) -> None:
+    writer = None
     try:
-        reader, writer = await asyncio.open_connection(host, port)
+        reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=1)
         writer.write(b'kek\r\n\r\n')
         await writer.drain()
         data = await reader.read(1024)
@@ -13,10 +14,11 @@ async def tcp_scanner(port, host):
             print(f'data: {data}')
         print(f'TCP Open {port} {define_protocol(data)}')
     finally:
-        writer.close()
-        await writer.wait_closed()
+        if writer is not None:
+            writer.close()
+            await writer.wait_closed()
 
-async def udp_scanner(port, host):
+async def udp_scanner(port: int, host: str) -> None:
     try:
         reader, writer = await asyncio.open_connection(host, port, proto=asyncio.DatagramProtocol)
         writer.write(b'kek')
@@ -75,5 +77,8 @@ async def main():
 
 if __name__ == '__main__':
     start_time = time.time()
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(e)
     print(f'Execution time: {time.time() - start_time:.2f} seconds')
