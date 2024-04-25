458a78c9421c0c282ddff5f60e52c6e635dbe70f
Sergey Z <cergeypro703@gmail.com>
2024-04-25 16:17:40 +0500

    works todo: refactor and cleanup

--- portscan/scaner_asyncio.py
+++ portscan/scaner_asyncio.py
@@ -1,22 +1,33 @@
+import logging
 import sys
-import socket
 import asyncio
 import time
+from asyncio import Queue
+
+MAX_CONNECTIONS = 1000 
 
 async def tcp_scanner(port: int, host: str) -> None:
-    writer = None
     try:
-        reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=1)
+        reader, writer = await asyncio.wait_for( asyncio.open_connection(host, port), timeout=0.5)
         writer.write(b'kek\r\n\r\n')
-        await writer.drain()
+        # await writer.drain()
         data = await reader.read(1024)
         if data:
             print(f'data: {data}')
         print(f'TCP Open {port} {define_protocol(data)}')
-    finally:
-        if writer is not None:
-            writer.close()
-            await writer.wait_closed()
+        writer.close()
+        await writer.wait_closed()
+    except ConnectionRefusedError as e:
+        logging.error(f'Connection refused {port}: {e}')
+    except TimeoutError as exp:
+        logging.error(f'Timeout Error123 scanning port {port}. Exception: {exp}')
+    except asyncio.TimeoutError as exp:
+        logging.error(f'Timeout Error scanning port {port}. Exception: {exp}')
+
+    except Exception as e:
+        logging.error(f'Error scanning port {port}. Exception: {e}')
+        # print(f'type(e): {type(e)}')
+        
 
 async def udp_scanner(port: int, host: str) -> None:
     try:
@@ -27,9 +38,11 @@ async def udp_scanner(port: int, host: str) -> None:
         if data:
             print(f'data: {data}')
         print(f'UDP Open {port} {define_protocol(data)}')
-    finally:
         writer.close()
         await writer.wait_closed()
+    except Exception as e:
+        pass
+        # print(f'Error scanning port {port}: {e}')
 
 def define_protocol(data: bytes) -> str:
     protocol = 'Unknown'
@@ -49,6 +62,17 @@ def define_protocol(data: bytes) -> str:
         protocol = 'SSH'
     return protocol
 
+async def worker(queue: Queue, host: str) -> None:
+    while True:
+        port = await queue.get()
+
+        # print(f"Starting scanning on host: {host} port: {port} ")
+        
+        await tcp_scanner(int(port), host)
+        # await udp_scanner(port, host)
+        queue.task_done()
+        # print(f"Finished scanning on host: {host} port: {port} ")
+
 async def main():
     if len(sys.argv) == 5:
         start_port = int(sys.argv[1])
@@ -59,18 +83,21 @@ async def main():
         protocol = sys.argv[3]
         host = sys.argv[4]
         
-        tasks = []
-        if protocol == 'TCP':
-            for port in range(start_port, finish_port + 1):
-                tasks.append(tcp_scanner(port, host))
-        elif protocol == 'UDP':
-            for port in range(start_port, finish_port + 1):
-                tasks.append(udp_scanner(port, host))
-        else:
-            print('Неправильный протокол')
-            sys.exit(-1)
+        queue = Queue()
+        for port in range(start_port, finish_port + 1):
+            await queue.put(port)
         
-        await asyncio.gather(*tasks)
+        tasks = []
+        for _ in range(MAX_CONNECTIONS):
+            tasks.append(asyncio.create_task(worker(queue, host)))
+        
+        await queue.join()  # Ожидание завершения всех задач в очереди
+        
+        for task in tasks:
+            task.cancel()
+        
+        await asyncio.gather(*tasks, return_exceptions=True)  # Ожидание завершения всех задач
+        
     else:
         print('Неправильный ввод аргументов')
         sys.exit(-1)
