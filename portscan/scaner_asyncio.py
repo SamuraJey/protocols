6884a3f27f5b832582907ff2f8ef620729257bf7
SamuraJey <cergeypro703@gmail.com>
2024-04-28 06:43:26 +0500

    well i hope it works.

--- portscan/scaner_asyncio.py
+++ portscan/scaner_asyncio.py
@@ -1,3 +1,5 @@
+import json
+import argparse
 import logging
 import socket
 import struct
@@ -5,65 +7,134 @@ import sys
 import asyncio
 import time
 from asyncio import Queue
-import asyncio_udp as asudp
 
-MAX_CONNECTIONS = 1500 
+MAX_CONNECTIONS = 1500
 closed_ports = []
 open_ports = []
-async def tcp_scanner(port: int, host: str) -> None:
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Сканер портов TCP и UDP')
+    parser.add_argument('host', type=str, help='IP адрес хоста')
+    parser.add_argument('-t', '--tcp', action='store_true',
+                        help='Сканировать TCP порты')
+    parser.add_argument('-u', '--udp', action='store_true',
+                        help='Сканировать UDP порты')
+    parser.add_argument('-p', '--ports', nargs=2, type=int,
+                        metavar=('N1', 'N2'), help='Диапазон портов для сканирования')
+
+    return parser.parse_args()
+
+
+def check_is_admin():
     try:
-        reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=0.5)
+        sock = socket.socket(
+            socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        sock.close()
+        return True
+    except PermissionError:
+        return False
+
+
+def check_icmp_code_is_3(data: bytes) -> bool:
+    icmp_header = data[20:28]
+    # Проверяем, является ли полученный пакет ICMP пакетом 'Port Unreachable'
+    icmp_type, code, checksum, id, seq = struct.unpack(
+        '!bbHHh', icmp_header)
+    if icmp_type == 3 or code == 3:
+        return True
+    else:
+        return False
+
+
+async def tcp_scanner(port: int, host: str, well_known_ports: dict) -> None:
+    try:
+
+        reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port), timeout=1)
         writer.write(b'kek\r\n\r\n')
-        # await writer.drain()
+        await writer.drain()
         data = await reader.read(1024)
-        if data:
-            print(f'data: {data.decode()}')
-        print(f'TCP Open {port} {define_protocol(data)}')
-        # open_ports.append(port)
+        # if data:
+        #     print(f'data: {data.decode()}')
+
+        guessed_protocol = define_protocol(data)
+        if guessed_protocol == 'Unknown':
+            try:
+                guessed_protocol = well_known_ports.get(str(port))
+            except KeyError:
+                guessed_protocol = 'Unknown'
+        if not guessed_protocol:
+            guessed_protocol = 'Unknown'
+        print(f'TCP Open {port} protocol is {guessed_protocol}')
+        open_ports.append(port)
         writer.close()
         await writer.wait_closed()
     except TimeoutError as expt:
-        # logging.error(f'Timeout Error while scanning port {port}. Port probably closed.')
+        pass
+        # print(
+        #     f'Timeout Error while scanning port {port}. Port probably closed.')
         # closed_ports.append(port)
         pass
+    except ConnectionRefusedError:
+        # print(f'Connection refused while scanning port {port}.\
+        #         Port probably closed.')
+        closed_ports.append(port)
+    except OSError as e:
+        # Оно почему-то выкидывет какой-то OSError, но я не могу понять какой конкретно, пока так, потом мб пофиксим
+        pass
+        # print(f'Connection refused while scanning port {port}. Port probably closed.')
     except Exception as e:
-        logging.error(f'Error while scanning port {port}. Exception description: {e} Exception type: {type(e)}')
+        logging.error(f'Error while scanning port {port}.\
+                       Exception description: {e} Exception type: {type(e)}')
 
 
-async def udp_scanner(port, host):
+async def udp_scanner(port: int, host: str, well_known_ports: dict) -> None:
     try:
         # Создаем raw socket для получения ICMP пакетов
-        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-        sock.settimeout(2)
+        sock = socket.socket(
+            socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+        sock.settimeout(1.5)
 
         # Создаем UDP-сокет для отправки пустых пакетов
         udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        udp_sock.settimeout(2)
+        udp_sock.settimeout(1.5)
 
-        # Отправляем пустой UDP-пакет
+        # Отправляем UDP-пакет
         await asyncio.get_event_loop().sock_sendto(udp_sock, b'wdkomwokweokdwdewd', (host, port))
 
         try:
             # Пытаемся получить ответ от сервера
             data, addr = await asyncio.get_event_loop().sock_recvfrom(sock, 1024)
-            icmp_header = data[20:28]
-            # Проверяем, является ли полученный пакет ICMP пакетом "Port Unreachable"
-            icmp_type, code, checksum, id, seq = struct.unpack('!bbHHh', icmp_header)
-            if icmp_type == 3 or code == 3:
-                print(f"Port {port} is closed")
-            else:
-                print(f"Port {port} is open")
+            if check_icmp_code_is_3(data):
+                pass
+                # print(f'Port {port} is closed')
         except socket.timeout:
-            # Если нет ответа, то порт открыт
-            print(f"Port {port} is open")
+            # Если нет ответа, то отправляем еще один UDP-запрос
+            try:
+                await asyncio.get_event_loop().sock_sendto(udp_sock, b'wdkomwokweokdwdewd', (host, port))
+                data, addr = await asyncio.get_event_loop().sock_recvfrom(sock, 1024)
+                if check_icmp_code_is_3(data):
+                    return
+                    # print(f'Port {port} is closed')
+            except socket.timeout:
+                try:
+                    print(
+                        f'UDP Port {port} is probably open and probably belongs to {well_known_ports.get(str(port))}')
+                except KeyError:
+                    print(
+                        f'UDP Port {port} is probably open, but application protocol is Unknown')
+
     except Exception as e:
-        print(f"Error scanning port123 {port}: {e}")
+        print(f'Error scanning port {port}: {e}')
+
 
 def define_protocol(data: bytes) -> str:
     protocol = 'Unknown'
     if b'SMTP' in data:
         protocol = 'SMTP'
-    elif b'POP3' in data or b'+OK' in data or b'-ERR' in data or b'USER' in data or b'PASS' in data or b'STAT' in data or b'LIST' in data or b'RETR' in data or b'TOP' in data or b'DELE' in data or b'QUIT' in data:
+    elif b'POP3' in data or b'+OK' in data or b'-ERR' in data or b'USER' in data\
+            or b'PASS' in data or b'STAT' in data or b'LIST' in data \
+            or b'RETR' in data or b'TOP' in data or b'QUIT' in data:
         protocol = 'POP3'
     elif b'IMAP' in data or b'* OK' in data:
         protocol = 'IMAP'
@@ -77,45 +148,56 @@ def define_protocol(data: bytes) -> str:
         protocol = 'SSH'
     return protocol
 
-async def worker(queue: Queue, host: str, mode: str) -> None:
+
+async def worker(queue: Queue, host: str, mode: str, well_known_ports: dict) -> None:
     while True:
+
         port = await queue.get()
-        # print(f"Starting scanning on host: {host} port: {port} ")
         if mode.lower() == 'tcp':
-            await tcp_scanner(int(port), host)
+            await tcp_scanner(int(port), host, well_known_ports)
         elif mode.lower() == 'udp':
-            await udp_scanner(int(port), host)
-        # await udp_scanner(int(port), host)
+            await udp_scanner(int(port), host, well_known_ports)
+
         queue.task_done()
-        # print(f"Finished scanning on host: {host} port: {port} ")
+
 
 async def main():
-    if len(sys.argv) == 5:
-        start_port = int(sys.argv[1])
-        finish_port = int(sys.argv[2])
-        if start_port < 0 or start_port > 65535 or finish_port < 0 or finish_port > 65535:
-            print('Неверный номер порта')
-            sys.exit(-1)
-        protocol = sys.argv[3]
-        host = sys.argv[4]
-        
-        queue = Queue()
-        for port in range(start_port, finish_port + 1):
-            await queue.put(port)
-        
-        tasks = []
-        for _ in range(MAX_CONNECTIONS):
-            tasks.append(asyncio.create_task(worker(queue, host, "udp")))
-        
-        await queue.join()  # Ожидание завершения всех задач в очереди
-        
-        for task in tasks:
-            task.cancel()
+    with open('portscan/well_known_ports.json', 'r') as file:
+        well_known_ports = json.load(file)
 
-        await asyncio.gather(*tasks, return_exceptions=True)  # Ожидание завершения всех задач
+    args = get_args()
+    start_port = args.ports[0]
+    finish_port = args.ports[1]
+
+    if start_port < 0 or start_port > 65535 or finish_port < 0 or finish_port > 65535:
+        print('Неверный номер порта')
+        sys.exit(1)
+
+    if args.tcp:
+        protocol = 'tcp'
     else:
-        print('Неправильный ввод аргументов')
-        sys.exit(-1)
+        if not check_is_admin():
+            print(f'We need root privelegies to scan UDP sockets')
+            sys.exit(1)
+        protocol = 'udp'
+
+    host = args.host
+    queue = Queue()
+    for port in range(start_port, finish_port + 1):
+        await queue.put(port)
+
+    tasks = []
+    for _ in range(MAX_CONNECTIONS):
+        tasks.append(asyncio.create_task(
+            worker(queue, host, protocol, well_known_ports)))
+
+    await queue.join()  # Ожидание завершения всех задач в очереди
+
+    for task in tasks:
+        task.cancel()
+
+    # Ожидание завершения всех задач
+    await asyncio.gather(*tasks, return_exceptions=True)
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -123,7 +205,8 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except Exception as e:
-        logging.error(f'Error while running main. Exception description: |{e}| Exception type: {type(e)}')
+        logging.error(
+            f'Error while running main. Exception description: |{e}| Exception type: {type(e)}')
     end_time = time.time()
 
     print(f'Execution time: {(end_time - start_time):.2f} seconds')
